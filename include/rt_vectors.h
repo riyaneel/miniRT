ac29de98cab156427286061f216957d6c3096f57
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-06 20:17:44 +0100

    feat(vectors): Add detailed arithmetic operations and SIMD optimizations in rt_vectors.h

--- include/rt_vectors.h
+++ include/rt_vectors.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 13:43:27 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/12/06 08:07:54 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/06 20:17:07 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,26 +57,65 @@ typedef union u_vec4 {
  */
 typedef t_vec4	t_vec3;
 
+/**
+ * @brief Initializes a new vector with specific components.
+ * @param x The x component.
+ * @param y The y component.
+ * @param z The z component.
+ * @param w The w component (usually 0 for a direction, 1 for a point).
+ * @return The initialized vector.
+ */
 static inline t_vec4	vec_init(float x, float y, float z, float w)
 {
 	return ((t_vec4){x, y, z, w});
 }
 
+/**
+ * @brief Performs component-wise addition of two vectors.
+ * @note Uses SIMD instructions for parallel processing.
+ * @param a The first vector.
+ * @param b The second vector.
+ * @return Result where r.x = a.x + b.x, etc.
+ */
 static inline t_vec4	vec4_add(const t_vec4 a, const t_vec4 b)
 {
 	return ((t_vec4){a.v + b.v});
 }
 
+/**
+ * @brief Performs component-wise substraction of two vectors.
+ * @note Uses SIMD instructions for parallel processing.
+ * @param a The first vector.
+ * @param b The second vector.
+ * @return Result where r.x = a.x - b.x, etc.
+ */
 static inline t_vec4	vec4_sub(const t_vec4 a, const t_vec4 b)
 {
 	return ((t_vec4){a.v - b.v});
 }
 
+/**
+ * @brief Performs component-wise multiplication (Hadamard Product) of
+ *  two vectors.
+ * @note Uses SIMD instructions for parallel processing.
+ * @warning This is not a dot product or cross-product.
+ * @param a The first vector.
+ * @param b The second vector.
+ * @return Result where r.x = a.x + b.x, etc.
+ */
 static inline t_vec4	vec4_mul(const t_vec4 a, const t_vec4 b)
 {
 	return ((t_vec4){a.v * b.v});
 }
 
+/**
+ * @brief Performs component-wise division of two vectors.
+ * @note Uses SIMD instructions for parallel processing.
+ * @warning Division by zero will result in Infinity or NaN components.
+ * @param a The first vector.
+ * @param b The second vector.
+ * @return Result where r.x = a.x + b.x, etc.
+ */
 static inline t_vec4	vec4_div(const t_vec4 a, const t_vec4 b)
 {
 	return ((t_vec4){a.v / b.v});
