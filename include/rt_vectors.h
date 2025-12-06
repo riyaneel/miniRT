f091f322726a0126886c5e3b06e173ebf8dd3c2a
Riyane El Qoqui <riyane.dev@gmail.com>
2025-12-06 08:16:48 +0100

    feat(vectors): Refactor vector structures to use SIMD and improve operations

--- include/rt_vectors.h
+++ include/rt_vectors.h
@@ -6,7 +6,7 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 13:43:27 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2025/12/05 14:50:16 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2025/12/06 08:07:54 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,75 +14,72 @@
 # define RT_VECTORS_H
 
 /**
- * @struct s_vec4
- * @brief Represents a 4-dimensional math vector.
- * * This structure is the fundamental building block for the raytracer linear
- *	algebra.<br>
- * It is used to represent:
+ * @typedef t_v4sf
+ * @brief Internal type for vector extension (SIMD).
+ * <br>Contains 4 floats (16 bytes) processed simultaneously by the CPU.
+ */
+typedef float	t_v4sf __attribute__((vector_size(16)));
+
+/**
+ * @union u_vec4
+ * @brief Represents a 4-dimensional mathematical vector.
+ * <br>This structure is the fundamental building block for the raytracer
+ * linear algebra.
+ * <br>It allows data access either as a SIMD vector (v) or via individual
+ * components (x, y, z, w).
+ * <br>Typical uses:
  * - 3D points in space (where w is typically 1.0)
  * - 3D direction vectors (where w is typically 0.0)
- * - Colors (RGBA) or raw data for SIMD alignment
+ * - Colors (RGBA)
  * @note The 'w' component allows for matrix multiplication operations
- *  (projection, rotation, translation) using homogeneous coordinates
- * @authors Riyane
+ * (projection, rotation, translation) using homogeneous coordinates.
+ * @note The `aligned(16)` attribute is crucial to prevent segmentation faults
+ * when using SSE/AVX instructions.
+ * @author Riyane
  */
-typedef struct s_vec4 {
-	float	x;
-	float	y;
-	float	z;
-	float	w;
-}	t_vec4;
+typedef union u_vec4 {
+	t_v4sf	v;
+	struct {
+		float	x;
+		float	y;
+		float	z;
+		float	w;
+	};
+} __attribute__((aligned(16)))	t_vec4;
 
 /**
  * @typedef t_vec3
- * @brief Alias for t_vec4 used when contextually handling 3D data
- * * We use a 4D structure for 3D vectors to maintain memory alignment
- * (padding), which helps with compiler optimizations and vectorization (SIMD).
+ * @brief Alias for t_vec4 used when contextually handling 3D data.
+ * <br>Although we logically manipulate x, y, and z, we use a 4D structure to
+ * maintain memory alignment (padding).
+ * <br>This facilitates compiler optimizations and vectorization (SIMD), with
+ * the 'w' component being ignored or used for alignment.
  */
 typedef t_vec4	t_vec3;
 
+static inline t_vec4	vec_init(float x, float y, float z, float w)
+{
+	return ((t_vec4){x, y, z, w});
+}
+
 static inline t_vec4	vec4_add(const t_vec4 a, const t_vec4 b)
 {
-	t_vec4	final;
-
-	final.x = a.x + b.x;
-	final.y = a.y + b.y;
-	final.z = a.z + b.z;
-	final.w = a.w + b.w;
-	return (final);
+	return ((t_vec4){a.v + b.v});
 }
 
 static inline t_vec4	vec4_sub(const t_vec4 a, const t_vec4 b)
 {
-	t_vec4	final;
-
-	final.x = a.x - b.x;
-	final.y = a.y - b.y;
-	final.z = a.z - b.z;
-	final.w = a.w - b.w;
-	return (final);
+	return ((t_vec4){a.v - b.v});
 }
 
 static inline t_vec4	vec4_mul(const t_vec4 a, const t_vec4 b)
 {
-	t_vec4	final;
-
-	final.x = a.x * b.x;
-	final.y = a.y * b.y;
-	final.z = a.z * b.z;
-	final.w = a.w * b.w;
-	return (final);
+	return ((t_vec4){a.v * b.v});
 }
 
 static inline t_vec4	vec4_div(const t_vec4 a, const t_vec4 b)
 {
-	t_vec4	final;
-
-	final.x = a.x / b.x;
-	final.y = a.y / b.y;
-	final.z = a.z / b.z;
-	final.w = a.w / b.w;
-	return (final);
+	return ((t_vec4){a.v / b.v});
 }
 
 #endif // RT_VECTORS_H
