7f5f2695a16648d523d7c3dafab6beaadda81365
Riyane El Qoqui <riyane.dev@gmail.com>
2026-01-06 13:51:28 +0100

    feat(allocator): Add arena allocator with configuration and alignment support

---
+++ include/allocator/arena_allocator.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arena_allocator.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 07:46:04 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2026/01/05 21:21:17 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARENA_ALLOCATOR_H
+# define ARENA_ALLOCATOR_H
+
+# include "arena_allocator_types.h"
+
+t_arena	*arena_create(t_arena_config config)
+		__attribute__((warn_unused_result));
+
+void	*arena_alloc(t_arena *arena, size_t size)
+		__attribute__((warn_unused_result, malloc, alloc_size(2)));
+
+void	*arena_alloc_align(t_arena *arena, size_t size, size_t alignment)
+		__attribute__((warn_unused_result, malloc, alloc_size(2)));
+
+void	arena_reset(t_arena *arena);
+
+void	arena_destroy(t_arena *arena);
+
+size_t	arena_get_used(const t_arena *arena);
+
+size_t	arena_get_capacity(const t_arena *arena);
+
+#endif // ARENA_ALLOCATOR_H
---
+++ include/allocator/arena_allocator_types.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arena_allocator_types.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/05 13:23:53 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2026/01/06 13:35:09 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARENA_ALLOCATOR_TYPES_H
+# define ARENA_ALLOCATOR_TYPES_H
+
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdint.h>
+
+# define ARENA_DEF_ALIGN 16
+
+typedef struct s_arena_config {
+	int64_t	capacity;
+	int32_t	oom_policy;
+	int32_t	reserved;
+} __attribute__((aligned(16)))	t_arena_config;
+
+typedef struct s_arena {
+	uintptr_t	current_ptr;
+	uintptr_t	end_ptr;
+	uintptr_t	base_ptr;
+	size_t		total_size;
+} __attribute__((aligned(32)))	t_arena;
+
+#endif // ARENA_ALLOCATOR_TYPES_H
