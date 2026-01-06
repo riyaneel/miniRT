0d33fe6b4fc11be7699e71921615573d7b14efd7
Riyane El Qoqui <riyane.dev@gmail.com>
2026-01-06 15:15:36 +0100

    feat(core): Add core initialization with register configuration

---
+++ src/core/core_init.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   core_init.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/17 17:04:57 by rel-qoqu          #+#    #+#             */
+/*   Updated: 2025/12/17 17:15:55 by rel-qoqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <xmmintrin.h>
+
+#include "core/rt_core.h"
+
+static inline void	configure_registers(void)
+{
+	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+	_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+}
+
+bool	core_init(void)
+{
+	configure_registers();
+	return (true);
+}
