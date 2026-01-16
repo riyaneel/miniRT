a8341ac217e5d5cfd42aeca521af48dcad82bb87
Riyane El Qoqui <riyane.dev@gmail.com>
2026-01-16 08:09:27 +0100

    refactor(shader): modularize shading logic and add light calculation helper

--- include/raytracer/shader.h
+++ include/raytracer/shader.h
@@ -6,50 +6,78 @@
 /*   By: rel-qoqu <rel-qoqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 03:44:58 by rel-qoqu          #+#    #+#             */
-/*   Updated: 2026/01/12 20:12:26 by rel-qoqu         ###   ########.fr       */
+/*   Updated: 2026/01/16 08:08:51 by rel-qoqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHADER_H
 # define SHADER_H
 
-# include "raytracer/world.h"
+# include "world.h"
 # include "utils/maths_utils.h"
 
-static inline t_vec4	shade_ray(const t_scene *scn, const t_ray *r, const t_hit *rec)
-{
-	t_vec4	ambient;
-	t_vec4	diffuse;
-	t_vec4	specular;
-	t_vec4	light_dir;
-	t_vec4	view_dir;
-	t_vec4	reflect_dir;
-	float	light_dist;
-	float	diff_strength;
-	float	spec_strength;
-	t_ray	shadow_ray;
-	t_vec4	light_vec;
+typedef struct s_light_calc {
+	t_vec4	dir;
+	float	dist;
+	float	padding[3];
+}	t_light_calc;
 
-	ambient = vec4_mul(scn->ambient.color, rec->color_obj);
-	ambient = vec4_scale(ambient, scn->ambient.ratio);
-	light_vec = vec4_sub(scn->light.origin, rec->p);
-	light_dist = sqrtf(vec4_len_sq(light_vec));
-	light_dir = vec4_scale(light_vec, 1.0f / light_dist);
-	shadow_ray = ray_create(vec4_add(rec->p, vec4_scale(rec->normal, EPSILON)),
-			light_dir);
-	if (hit_world_any(scn, &shadow_ray, light_dist - EPSILON))
-		return (ambient);
+static inline t_light_calc	get_light_calc(const t_scene *scn, const t_vec4 p)
+{
+	t_light_calc	lc;
+	t_vec4			lv;
+
+	lv = vec4_sub(scn->light.origin, p);
+	lc.dist = sqrtf(vec4_len_sq(lv));
+	lc.dir = vec4_scale(lv, 1.0f / lc.dist);
+	return (lc);
+}
+
+static inline t_vec4	compute_diffuse(const t_scene *scn, const t_hit *rec,
+		const t_vec4 light_dir)
+{
+	float	diff_strength;
+	t_vec4	diffuse;
+
 	diff_strength = fmax_fast(vec4_dot(rec->normal, light_dir), 0.0f);
 	diffuse = vec4_mul(scn->light.color, rec->color_obj);
-	diffuse = vec4_scale(diffuse, scn->light.ratio * diff_strength);
+	return (vec4_scale(diffuse, scn->light.ratio * diff_strength));
+}
+
+static inline t_vec4	compute_specular(const t_scene *scn, const t_ray *r,
+		const t_hit *rec, const t_vec4 light_dir)
+{
+	t_vec4	view_dir;
+	t_vec4	reflect_dir;
+	float	spec_strength;
+
 	view_dir = vec4_scale(r->dir, -1.0f);
 	reflect_dir = vec4_sub(vec4_scale(rec->normal, 2.0f * vec4_dot(rec->normal,
 					light_dir)), light_dir);
 	spec_strength = powf(fmax_fast(vec4_dot(view_dir, reflect_dir), 0.0f),
 			32.0f);
-	specular = vec4_scale(scn->light.color, scn->light.ratio * spec_strength
-			* 0.6f);
-	return (vec4_add(ambient, vec4_add(diffuse, specular)));
+	return (vec4_scale(scn->light.color, scn->light.ratio
+			* spec_strength * 0.6f));
+}
+
+static inline t_vec4	shade_ray(const t_scene *scn, const t_ray *r,
+		const t_hit *rec)
+{
+	t_vec4			ambient;
+	t_light_calc	lc;
+	t_ray			shadow_ray;
+	t_vec4			phong;
+
+	ambient = vec4_scale(vec4_mul(scn->ambient.color, rec->color_obj),
+			scn->ambient.ratio);
+	lc = get_light_calc(scn, rec->p);
+	shadow_ray = ray_create(vec4_add(rec->p,
+				vec4_scale(rec->normal, EPSILON)), lc.dir);
+	if (hit_world_any(scn, &shadow_ray, lc.dist - EPSILON))
+		return (ambient);
+	phong = vec4_add(compute_diffuse(scn, rec, lc.dir),
+			compute_specular(scn, r, rec, lc.dir));
+	return (vec4_add(ambient, phong));
 }
 
 #endif // SHADER_H
